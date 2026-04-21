16c02393866e83a8a94c398cc068f2dbda19d4cf
Bartek Szkola <bartek326@icloud.com>
2026-04-21 20:55:15 +0200

    change CsvLoader structure

--- src/CsvFileLoader.java
+++ src/CsvFileLoader.java
@@ -2,29 +2,45 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CsvFileLoader {
 
-    public static List<Observation> loadFile(String fileName) {
+    public List<Observation> loadData(String fileName) {
 
-        List<Observation> observations = new ArrayList<>();
-        String[] parts;
+        List<Observation> observations = new ArrayList<Observation>();
+        String[] header = null;
 
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+
             String line;
 
-            while ((line = br.readLine()) != null) {
-                parts = line.split(",");
-                String[] attributes = new String[parts.length];
-                for (int i = 1; i < parts.length; i++) {
-                    attributes[i - 1] = parts[i];
-                }
-                observations.add(new Observation(parts[0], attributes));
+            if((line = br.readLine()) != null){
+                header = line.split(",");
             }
+
+            if(header == null){
+                throw new IllegalArgumentException("CSV file does not contain header");
+            }
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                String type = parts[0];
+
+                Map<String, String> features = new HashMap<>();
+
+                for(int i = 1; i < parts.length; i++){
+                    features.put(header[i - 1], parts[i]);
+                }
+                observations.add(new Observation(type,features));
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return observations;
     }
 }
